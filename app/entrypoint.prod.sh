792bc54e4eb5c66fb2fd63bb062edb0b66666cf5
SuperSteban <jestebansantti@gmail.com>
2024-09-22 23:15:09 -0700

    scaffolding Django Api rest containerized

---
+++ app/entrypoint.prod.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $HOST_DB $PORT_DB; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+exec "$@"
